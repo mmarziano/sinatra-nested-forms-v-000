ff04d2ea4a8989597a595123a80387c7361d7d82
Monica Marziano <mmarziano1@gmail.com>
2019-05-04 10:34:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,17 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
-
+    get '/' do 
+      erb :"/pirates/instructions"
+    end 
+  
+    get '/new' do 
+      erb :"/pirates/new"
+    end
+    
+    post '/pirates' do 
+      #binding.pry
+      erb :"/pirates/show"
+    end 
   end
 end
