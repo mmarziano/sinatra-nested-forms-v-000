cd0a3ff6b194b0ab1f24ba86e3a9e9e4b1fb1e16
Monica Marziano <mmarziano1@gmail.com>
2019-05-04 09:51:18 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  
+  attr_accessor :name, :type, :booty 
+  
+  SHIPS = []
+  
+  def initialize(name: params[:pirate][][:name], booty: params[:pirate][][:booty], type: params[:pirate][][:type])
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    SHIPS << self
+  end 
+  
+  def self.all 
+    SHIPS.all
+  end 
+  
+  def self.clear 
+    SHIPS.clear
+  end 
 end
